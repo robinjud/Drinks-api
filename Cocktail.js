8209f1fdc68c5a17d345b6fab5b506c2e1527715
Robinjud <rbnjd7@gmail.com>
2023-06-21 18:22:31 -0500

    Drink api

--- Cocktail.js
+++ Cocktail.js
@@ -1,33 +1,10 @@
-let form = document.querySelector (".js-cocktail-form");
-let cocktailInput = document.querySelector("[name=cocktail-name]");
-let responseContainer = document.querySelector(".js-response-container");
+let form = document.querySelector(".js-cocktail-form");
+let drinkInput = document.querySelector("[name=cocktail-name]"); 
 
-
-function displayCocktail(response){
-    const drinks = response.drinks;
-
-    let html = "";
-    for (let drink of drinks) {
-       html += '<p> ${cocktail.strDrink} </p>';
-           
-    }
-    responseContainer.innerHTML= html;
-}
-
-
-
-function quaryCocktail(searchExpression) {
-    const url= "www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchExpression;
-
-    fetch(url)
-        .then(data => data.json())
-        .then(displayCocktail);
-}
 
 function formSubmitted(event) {
-   event.preventDefault();
-   
-   quaryCocktail(cocktailInput.value);
-   cocktailInput.value = '';
+   event.preventDefault();   
+   alert(drinkInput.value);
 }
+
 form.addEventListener("submit", formSubmitted);
