c3bf5e057749152a740ac7141e182b9b34165d78
Robinjud <rbnjd7@gmail.com>
2023-07-06 17:32:21 -0500

    html display

--- Cocktail.js
+++ Cocktail.js
@@ -1,40 +1,36 @@
 let form = document.querySelector(".js-cocktail-form");
-let drinkInput = document.querySelector("[name=cocktail-name]"); 
+let alcoholInput = document.querySelector("[name=cocktail-name]"); 
 let responseContainer = document.querySelector(".js-response-container"); 
 
 
-function displayCocktails(response) {
-     const drinks = response.drinks;
+function displayCocktail(response) {
+    const drinks = response.drinks;
 
-
-     let html="";
-     for (let drink of drinks) {
-        html +='<p>$ </p>';
-     }
-
+    let html = ""
+    for (let drinks of drinks) {
+        html += `<p> ${drinks.strDrink} </p>`; 
+     
+    }
      responseContainer.innerHTML = html;
 
 }
 
-
-
-function queryDrink(searchExpression){
-    console.log(searchExpression);
-    const url =+ 'www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita' + searchExpression;
+function queryAlcohol(searchExpression){
+    const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=" + searchExpression;
 
 
     fetch(url)
-      .then(data => data.json())
-      .then(displayCocktails);
-    
+      .then((data) => data.json())
+      .then(displayCocktail);
+
 }
 
 function formSubmitted(event) {
    event.preventDefault();   
   
 
-   queryDrink(drinkInput.value);
-   drinkInput.value = '';
+   queryAlcohol(alcoholInput.value);
+   alcoholInput.value = "";
 }
 
 form.addEventListener("submit", formSubmitted);
