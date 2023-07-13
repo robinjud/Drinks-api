32503ab84b0bfa3ed35ee32e0955c6c3f3853cc7
Robinjud <rbnjd7@gmail.com>
2023-07-12 19:57:57 -0500

    pictures

--- Cocktail.js
+++ Cocktail.js
@@ -2,13 +2,23 @@ let form = document.querySelector(".js-cocktail-form");
 let alcoholInput = document.querySelector("[name=cocktail-name]"); 
 let responseContainer = document.querySelector(".js-response-container"); 
 
+function getImageHtml(drink){
+   if(typeof drink.strDrinkThumb === 'string'){
+    return  `<img src="${drink.strDrinkThumb}" alt="${drink.strDrink}"/>`;
+   }
+   return "";
+}
+
 
 function displayCocktails(response) {
     const drink = response.drinks;
 
     let html = '';
     for (let drinks of drink) {
-        html += `<p> ${drinks.strDrink} </p>`; 
+        html += `<section>
+             <h2>${drinks.strDrink}</h2>
+             ${getImageHtml(drinks)}
+        </section>`; 
      
     }
      responseContainer.innerHTML = html;
